69662ec0d250586338132d0b309273c8cee3ba51
Jacob Au <jacob.au@uwaterloo.ca>
2020-11-21 11:23:16 -0500

    added a few Packages, organized stuff

--- foodfight/src/App.js
+++ foodfight/src/App.js
@@ -1,24 +1,18 @@
-import './App.css';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'; 
+import Restaurant from "./components/Restaurant";
+import styles from "./App.module.scss";
 
 function App() {
 
   const [data, setData] = useState("broken");
   useEffect(()=>{
     console.log("hello?");
-    axios
-      .get("http://localhost:5000/api")
-      .then((response) => {
-        let data  = response.data; 
-        setData(data);
-        console.log(data); 
-      })
-      .catch((error)=> {})
-      .finally(() => {console.log("done")});
   })
   return (
-    <div> {data ? data.message : "what"} </div>
+    <div className={styles.root}> 
+      <Restaurant /> 
+    </div>
   );
 }
 
---
+++ foodfight/src/hooks/useBusiness.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+// hook used to GET business data, and "parse it" 
+export default function useBusiness(){
+
+  let finalData = {
+    businessName: "placehold",
+    price: "placehold",
+    
+  }
+  return(finalData);
+}
