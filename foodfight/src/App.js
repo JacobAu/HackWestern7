124583101cfe2f9d2c22fd0f48002a73bd27263f
Jacob Au <jacob.au@uwaterloo.ca>
2020-11-21 02:43:00 -0500

    added backend

--- foodfight/src/App.js
+++ foodfight/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'; 
 
 function App() {
+
+  const [data, setData] = useState("broken");
+  useEffect(()=>{
+    console.log("hello?");
+    axios
+      .get("http://localhost:5000/api")
+      .then((response) => {
+        let data  = response.data; 
+        setData(data);
+        console.log(data); 
+      })
+      .catch((error)=> {})
+      .finally(() => {console.log("done")});
+  })
   return (
-    <div>hello</div>
+    <div> {data ? data.message : "what"} </div>
   );
 }
 
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const PORT = process.env.PORT || 5000; 
+
+app.use(cors());
+
+app.listen(PORT, () =>{
+  console.log(`Listening to ${PORT}`);
+})
+
+app.get('/api', (req, res) => {
+  res.send({message : "hello"});
+})
